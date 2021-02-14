c12e3945cd518ea370d5289ced98bedda24cbab8
Dev1 <Dev1@gmail.com>
2021-02-14 10:39:06 +0530

    modified by Dev1

--- ProjectTest1/src/com/nt/beans/Utility.java
+++ ProjectTest1/src/com/nt/beans/Utility.java
@@ -14,4 +14,16 @@ public class Utility {
 		System.out.println("abc123");
 //>>>>>>> branch 'master' of https://github.com/AamirNit/TechProject.git
 	}
+	public void m2() {
+		System.out.println("Utility.m2()");
+	}
+	public void m3() {
+		System.out.println("Utility.m3()");
+	}
+	public void m4() {
+		System.out.println("Utility.m4()");
+	}
+	public void m5() {
+		System.out.println("Utility.m5()");
+	}
 }
